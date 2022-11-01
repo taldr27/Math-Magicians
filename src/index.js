82dd70addbd82722309347f18c51242dd995ea3b
taldr27 <diegogarciaq98@gmail.com>
2022-11-01 11:50:03 -0500

    Add change to index

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import Calculator from './components/calculator';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Calculator />
   </React.StrictMode>,
 );
